36aea520ce3bc52c47c8da6450d217022d694f8e
“Olivier <gelinas_tp@hotmail.com>
2021-12-26 01:09:43 -0500

    kraken to helix

--- MMM-Twitch.js
+++ MMM-Twitch.js
@@ -12,15 +12,21 @@ Module.register("MMM-Twitch", {
 		updateInterval:	15*60*1000,
 		retryDelay: 	5000,
 		client_id:		null,
+		client_secret:	null,
 		channels: 		[]
 	},
 
 	requiresVersion: "2.1.0", // Required version of MagicMirror
 
+	
+
 	start: function() {
 		var self = this;
 		var dataRequest = null;
 		var dataNotification = null;
+		
+		this.access_token = null;
+		this.response = null;
 
 		//Flag for check if module is loaded
 		this.loaded = false;
@@ -32,17 +38,28 @@ Module.register("MMM-Twitch", {
 		}, this.config.updateInterval);
 	},
 
-	/*
-	 * getData
-	 * function example return data and show it in the module wrapper
-	 * get a URL request
-	 *
-	 */
+	getAccessToken: function() {
+		var self = this;
+		var http = new XMLHttpRequest();
+		var url = 'https://id.twitch.tv/oauth2/token?client_id=' + this.config.client_id + '&client_secret=' + this.config.client_secret + '&grant_type=client_credentials';
+		http.open('POST', url, true);
+
+		//Send the proper header information along with the request
+		http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+		http.onreadystatechange = function() {//Call a function when the state changes.
+			if(http.readyState == 4 && http.status == 200) {
+				var oauth2 = JSON.parse(http.responseText);
+				self.access_token = oauth2.access_token;
+			}
+		}
+		http.send();
+	},
 
 	getChannelsId: function() {
 		var channels_id = [];
 		this.config.channels.forEach(channel => {
-			channels_id.push(channel["channel_id"]);
+			channels_id.push("user_id=" + channel["channel_id"]);
 		})
 		return channels_id;
 	},
@@ -50,35 +67,26 @@ Module.register("MMM-Twitch", {
 	getData: function() {
 		var self = this;
 
+		// Retreive an array of id of the channels
 		var channels_id = this.getChannelsId();
 
-		console.log("channels_id.join()");
-		console.log(channels_id.join());
-
-		var urlApi = "https://api.twitch.tv/kraken/streams/?stream_type=all&channel=" + channels_id.join();
+		var urlApi = "https://api.twitch.tv/helix/streams?" + channels_id.join("&");
 		var retry = true;
-
-		console.log("urlApi");
-		console.log(urlApi);
 
 		var dataRequest = new XMLHttpRequest();
 		dataRequest.open("GET", urlApi, true);
 
+		dataRequest.setRequestHeader("Authorization", "Bearer " + this.access_token);
 		dataRequest.setRequestHeader("client-Id", this.config.client_id);
-		dataRequest.setRequestHeader("Accept", "application/vnd.twitchtv.v5+json");
 
 		dataRequest.onreadystatechange = function() {
-			console.log(this.readyState);
-			if (this.readyState === 4) {
-				console.log(this.status);
-				if (this.status === 200) {
-					self.processData(JSON.parse(this.response));
-				} else if (this.status === 401) {
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, this.status);
-					retry = false;
+			if (dataRequest.readyState === 4) {
+				if (dataRequest.status === 200) {
+					self.processData(JSON.parse(dataRequest.response).data);
+				} else if (dataRequest.status === 401) {
+					self.getAccessToken();
 				} else {
-					Log.error(self.name, "Could not load data.");
+					console.error(self.name, "Could not load data.");
 				}
 				if (retry) {
 					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
@@ -88,13 +96,6 @@ Module.register("MMM-Twitch", {
 		dataRequest.send();
 	},
 
-
-	/* scheduleUpdate()
-	 * Schedule next update.
-	 *
-	 * argument delay number - Milliseconds before next update.
-	 *  If empty, this.config.updateInterval is used.
-	 */
 	scheduleUpdate: function(delay) {
 		var nextLoad = this.config.updateInterval;
 		if (typeof delay !== "undefined" && delay >= 0) {
@@ -108,15 +109,15 @@ Module.register("MMM-Twitch", {
 	},
 
 	isOnline: function(channel, streams){
-		var status = false
-		if (streams["streams"].length > 0) {
-			streams["streams"].forEach(stream => {
-				if (channel["channel_id"] == stream["channel"]["_id"]) {
-					status = true;
+		var viewer_count = 0;
+		if (streams.length > 0) {
+			streams.forEach(stream => {
+				if (channel["channel_id"] == stream.user_id) {
+					viewer_count = stream.viewer_count;
 				}
 			});
 		}
-		return status;
+		return viewer_count;
 	},
 
 	getDom: function() {
@@ -126,44 +127,54 @@ Module.register("MMM-Twitch", {
 		if (this.config.client_id == null) {
 			var message = document.createElement("div");
 			message.classList.add("small", "bright");
-			message.innerHTML = "client_id must be specifiy";
+			message.innerHTML = "client_id and client_secret must be specifiy";
 			wrapper.appendChild(message);
 			return wrapper;
 		}
 
 		// If this.dataRequest is not empty
-		var streams = this.streams;
-		if (streams) {
+		
+		if (this.response) {
+
+			var twitch_wrapper = document.createElement("div");
+			wrapper.appendChild(twitch_wrapper);
+
+			var table = document.createElement("table");
+			twitch_wrapper.appendChild(table);
+			
 			var self = this;
+			var streams = self.response;
 			this.config.channels.forEach(channel => {
 				var isOnline = self.isOnline(channel, streams);
-				console.log(isOnline);
 
-				var stream = document.createElement("div");
-				stream.classList.add("small");
+				var tr = document.createElement("tr");
+				tr.classList.add("small");
+				table.appendChild(tr);
 
+				var status = document.createElement('td');
+				tr.appendChild(status);
 				var channel_status = document.createElement('div');
 				channel_status.classList.add("small", "channel_status");
+				tr.appendChild(channel_status);
 
-				var channel_name = document.createElement("span");
+				var channel_name = document.createElement("td");
 				channel_name.classList.add("small", "channel_name");
 				channel_name.innerHTML = channel["display_name"];
+				tr.appendChild(channel_name);
 				
-				if (isOnline) {
+				var viewers = document.createElement("td");
+				tr.appendChild(viewers);
+				if (isOnline > 0) {
 					//channel_status.src = this.file("icons/online.png");
 					channel_status.classList.add("channel_online");
 					channel_name.classList.add("bright");
+					viewers.innerHTML = isOnline;
 				} else {
 					//channel_status.src = this.file("icons/offline.png");
 					channel_status.classList.add("channel_offline");
 					channel_name.classList.add("light", "dimmed");
 				}
-
-				stream.appendChild(channel_status);
-
-				stream.appendChild(channel_name);
-
-				wrapper.appendChild(stream);
+				
 			});
 			
 		};
@@ -214,9 +225,8 @@ Module.register("MMM-Twitch", {
 		if(notification === "MMM-Twitch-Reciever") {
 			// set dataNotification
 			this.dataNotification = payload;
-			this.streams = payload;
-			console.log("MMM-Twitch-Reciever");
-			// console.log(payload);
+
+			this.response = payload;
 			this.updateDom();
 		}
 	},
